83f446849824012657bd0bee38172800f42abfe5
anthonyych4n <anthony168c@gmail.com>
2024-06-07 18:27:06 -0600

    Footer component

--- src/app/spreadsheet/page.tsx
+++ src/app/spreadsheet/page.tsx
@@ -1,11 +1,9 @@
 import MainSpreadsheet from "@/components/spreadsheet/MainSpreadsheet";
-import SpreadsheetTest from "@/components/spreadsheet/SpreadsheetTest";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <MainSpreadsheet />
-      <SpreadsheetTest />
     </main>
   );
 }
