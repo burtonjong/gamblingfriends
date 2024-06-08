0895a4569dfad780759b01012d39f893c48f8ae3
Burton Jong <burtonjong05@gmail.com>
2024-06-07 22:35:36 -0600

    small change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex text-2xl">Home Page for now</div>
+      <div className="flex flex-row justify-around">
+        <div>Replace with austin component</div>
+        <div>Replace with anthony component</div>
+      </div>
     </main>
   );
 }
