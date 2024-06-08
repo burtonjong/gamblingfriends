3c2923d51ab3d2eb35bec0aeee9ee53b50a1fa4f
Matthew <matthewlblue@gmail.com>
2024-06-07 18:48:35 -0600

    burton wants changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
 export default function Header() {
   return (
-    <div className="flex h-24 flex-row items-center justify-evenly">
-      <a href="http://localhost:3000">Home</a>
-      <a href="http://localhost:3000/spreadsheet">Spreadsheet</a>
-      <a href="http://localhost:3000/planner">Planner</a>
+    <div className="flex h-24 flex-row items-center justify-evenly bg-slate-500">
+      <a href="/spreadsheet">Spreadsheet</a>
+      <a href="/">Home</a>
+      <a href="/planner">Planner</a>
     </div>
   );
 }
