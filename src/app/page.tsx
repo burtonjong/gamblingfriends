fa5b875c03a2fb45a7fea6b5f1d171dd9022be2f
austinkimkimkim <108385291+austinkimkimkim@users.noreply.github.com>
2024-06-15 12:21:56 -0600

    Created podium (#7)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import Podium from "@/components/homepage/Podium";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex flex-row justify-around">
-        <div>Replace with austin component</div>
+        <div>
+          <Podium />
+        </div>
         <div>Replace with anthony component</div>
       </div>
     </main>
