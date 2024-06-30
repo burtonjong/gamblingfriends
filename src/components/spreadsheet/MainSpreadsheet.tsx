896fab965d3557a69022b6e5ed5eadfac37c945d
anthonyych4n <anthony168c@gmail.com>
2024-06-29 22:03:40 -0600

    i love burton jong

--- src/components/spreadsheet/MainSpreadsheet.tsx
+++ src/components/spreadsheet/MainSpreadsheet.tsx
@@ -1,12 +1,51 @@
+"use client";
+
+import { generateClient } from "aws-amplify/data";
+import DataTable, { TableColumn } from "react-data-table-component";
+
+import { useQuery } from "@tanstack/react-query";
+
+import { type Schema } from "../../../amplify/data/resource";
+
+const client = generateClient<Schema>();
+interface DataRow {
+  name: string;
+  earnings: number;
+  sesssionsAttend: number;
+}
+
+const columns: TableColumn<DataRow>[] = [
+  {
+    name: "Name",
+    selector: (row) => row.name,
+    sortable: true,
+  },
+  {
+    name: "Earnings",
+    selector: (row) => row.earnings,
+    sortable: true,
+  },
+  {
+    name: "Sessions Attended",
+    selector: (row) => row.sesssionsAttend,
+    sortable: true,
+  },
+];
+
+const data = [
+  { name: "Black Burton", earnings: 100, sesssionsAttend: 3 },
+  { name: "Yellow Austin", earnings: 150, sesssionsAttend: 5 },
+];
+
 export default function MainSpreadsheet() {
   return (
     <div className="flex w-full flex-row">
-      <div className="size-full bg-slate-300">
+      <div className="size-full">
         <div className="flex flex-row gap-4">
-          <button>All time</button>
-          <button>Sessions</button>
+          <button className="">Earnings</button>
+          <button className="">Sessions Attended</button>
         </div>
-        <div className="h-96 bg-slate-500"></div>
+        <DataTable columns={columns} data={data} />
       </div>
     </div>
   );
--- src/components/spreadsheet/SpreadsheetTest.tsx
+++
@@ -1,3 +0,0 @@
-export default function SpreadsheetTest() {
-  return <div>i love black people</div>;
-}
