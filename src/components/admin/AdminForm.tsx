54195c3a20741a093679ff8d165ca4f498c7d19a
austinkimkimkim <austinkimkimkim@gmail.com>
2024-07-01 17:07:12 -0600

    Created session form

--- src/components/admin/AdminForm.tsx
+++ src/components/admin/AdminForm.tsx
@@ -2,12 +2,12 @@
 
 import { generateClient } from "aws-amplify/data";
 import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 
 import { type Schema } from "@/../amplify/data/resource";
 
 const client = generateClient<Schema>();
 
-//we need to assign a list of these User objects to the useState
 type User = {
   id: string;
   email: string;
@@ -17,7 +17,7 @@ type User = {
   totalEarnings?: number;
   sessionsAttended: SessionAttended[];
 };
-//referenced by User objects
+
 type SessionAttended = {
   id: string;
   sessionAttendedId: string;
@@ -25,17 +25,22 @@ type SessionAttended = {
   date: string;
 };
 
+type SessionFormData = {
+  sessionAttendedId: string;
+  earningsThatSession: number;
+  date: string;
+};
+
 export default function AdminForm() {
   const [users, setUsers] = useState<User[]>([]);
   const [searchQuery, setSearchQuery] = useState<string>("");
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
+  const { register, handleSubmit, reset } = useForm<SessionFormData>();
 
-  //to get all the users in our database
   useEffect(() => {
     async function fetchUsers() {
       try {
         const response = await client.models.User.list();
-        //console.log("response from client.models.User.list(): ")
-        //console.log(response)
         const userList = response.data as unknown as User[];
         setUsers(userList);
       } catch (error) {
@@ -46,13 +51,26 @@ export default function AdminForm() {
     fetchUsers();
   }, []);
 
-  const handleUserClick = (userId: string) => {
-    //TODO: when click on a user, make a new form on the right to add a new SessionAttended to the person
-    console.log(`User clicked: ${userId}`);
+  const handleUserClick = (user: User) => {
+    setSelectedUser(user);
+    reset(); // Reset the form when a new user is selected
   };
 
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchQuery(event.target.value);
+  };
+
+  const onSubmit = async (data: SessionFormData) => {
+    try {
+      await client.models.SessionsAttended.create({
+        ...data,
+        sessionAttendedId: selectedUser!.id,
+      });
+      // Optionally refetch users or update local state
+      console.log("Session added:", data);
+    } catch (error) {
+      console.error("Error creating session:", error);
+    }
   };
 
   const filteredUsers = users.filter((user) =>
@@ -62,25 +80,50 @@ export default function AdminForm() {
   );
 
   return (
-    <div>
-      <p>Admin</p>
-      <input //the search bar
-        type="text"
-        placeholder="Search for a name"
-        value={searchQuery}
-        onChange={handleSearchChange}
-      />
-      <ul>
-        {filteredUsers.map((user) => (
-          <li
-            key={user.id}
-            onClick={() => handleUserClick(user.id)}
-            style={{ cursor: "pointer" }}
-          >
-            {user.firstName} {user.lastName} ({user.email}) - Role: {user.role}
-          </li>
-        ))}
-      </ul>
+    <div style={{ display: "flex" }}>
+      <div>
+        <p>Admin</p>
+        <input
+          type="text"
+          placeholder="Search for a name"
+          value={searchQuery}
+          onChange={handleSearchChange}
+        />
+        <ul>
+          {filteredUsers.map((user) => (
+            <li
+              key={user.id}
+              onClick={() => handleUserClick(user)}
+              style={{ cursor: "pointer" }}
+            >
+              {user.firstName} {user.lastName} ({user.email}) - Role:{" "}
+              {user.role}
+            </li>
+          ))}
+        </ul>
+      </div>
+      {selectedUser && (
+        <div style={{ marginLeft: "20px" }}>
+          <h3>
+            Add Session for {selectedUser.firstName} {selectedUser.lastName}
+          </h3>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div>
+              <label>Date</label>
+              <input type="date" {...register("date", { required: true })} />
+            </div>
+            <div>
+              <label>Earnings</label>
+              <input
+                type="number"
+                step="0.01"
+                {...register("earningsThatSession", { required: true })}
+              />
+            </div>
+            <button type="submit">Add Session</button>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
