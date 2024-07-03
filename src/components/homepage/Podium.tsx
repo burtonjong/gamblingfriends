c9d14083af8e8800e5bbd68339dd76c1d3d3d8d9
Anthony <88424536+anthonyych4n@users.noreply.github.com>
2024-07-02 20:13:48 -0600

    DB connected to podium (#20)
    * DB connected to podium
    
    * Finished making changes
    
    ---------
    
    Co-authored-by: austinkimkimkim <austinkimkimkim@gmail.com>
    Co-authored-by: Burton Jong <108391733+burtonjong@users.noreply.github.com>

--- src/components/homepage/Podium.tsx
+++ src/components/homepage/Podium.tsx
@@ -1,53 +1,57 @@
 "use client";
 
+import { generateClient } from "aws-amplify/data";
 import { useState } from "react";
 
-const people = {
-  Austin: {
-    profit: 9000,
-    daysAttended: 2,
-  },
-  Burton: {
-    profit: 15,
-    daysAttended: 5,
-  },
-  Matthew: {
-    profit: -35,
-    daysAttended: 5,
-  },
-  Anthony: {
-    profit: 45,
-    daysAttended: 10,
-  },
-  Alex: {
-    profit: -55,
-    daysAttended: 5,
-  },
-};
+import { useQuery } from "@tanstack/react-query";
 
-const sortProfit = (sortType: boolean) => {
-  const peopleArray = Object.entries(people);
-  if (sortType) {
-    peopleArray.sort(
-      ([, personA], [, personB]) => personB.profit - personA.profit,
-    );
-  } else {
-    peopleArray.sort(
-      ([, personA], [, personB]) =>
-        personB.profit / personB.daysAttended -
-        personA.profit / personA.daysAttended,
-    );
-  }
-  const sortedPeople: Record<string, { profit: number; daysAttended: number }> =
-    {};
-  peopleArray.forEach(([name, data]) => {
-    sortedPeople[name] = data;
-  });
-  return sortedPeople;
-};
+import { type Schema } from "../../../amplify/data/resource";
+
+const client = generateClient<Schema>();
 
 export default function Podium() {
   const [rankType, setRankType] = useState(true);
+  const { data, isFetching } = useQuery({
+    initialDataUpdatedAt: 0,
+    queryKey: ["Users", {}],
+    queryFn: async () => {
+      try {
+        const usersResponse = await client.models.User.list();
+        const usersWithSessions = await Promise.all(
+          usersResponse.data.map(async (user) => {
+            const sessionsResponse = await client.models.SessionsAttended.list({
+              filter: { sessionAttendedId: { eq: user.id } },
+            });
+            return {
+              ...user,
+              sessionsAttended: sessionsResponse.data,
+            };
+          }),
+        );
+        return usersWithSessions;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  });
+
+  const sortProfit = (sortType: boolean) => {
+    try {
+      if (sortType) {
+        return data?.sort(
+          (a, b) => (b?.totalEarnings ?? 0) - (a?.totalEarnings ?? 0),
+        );
+      } else {
+        return data?.sort(
+          (a, b) =>
+            (b?.totalEarnings ?? 0) / (b.sessionsAttended.length || 1) -
+            (a?.totalEarnings ?? 0) / (a.sessionsAttended.length || 1),
+        );
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div className="mx-5 w-80 bg-slate-500 p-5">
@@ -70,27 +74,34 @@ export default function Podium() {
           <div>Name</div>
           <div>Profit</div>
         </div>
-        {Object.entries(
-          rankType ? sortProfit(rankType) : sortProfit(rankType),
-        ).map(([name, data], index) => (
-          <div
-            key={name}
-            className="m-5 flex items-center justify-between rounded-lg bg-slate-200 p-2"
-          >
-            <div
-              className={`size-10 ${index >= 3 ? "bg-slate-400" : index === 2 ? "bg-yellow-700" : index === 1 ? "bg-gray-400" : "bg-yellow-400"} mr-4 flex items-center justify-center rounded-full font-bold text-white`}
-            >
-              {index + 1}
-            </div>
-            <div className="m-2 font-bold">{name}</div>
-            <div className="font-bold">
-              {data.profit < 0 ? "-" : ""}$
-              {rankType
-                ? Math.abs(data.profit)
-                : Math.abs(data.profit / data.daysAttended) + "/day"}
-            </div>
-          </div>
-        ))}
+        <div>
+          {isFetching ? (
+            <h1>Loading.....</h1>
+          ) : (
+            sortProfit(rankType)?.map((user, index) => (
+              <div
+                key={user.id}
+                className="m-5 flex items-center justify-between rounded-lg bg-slate-200 p-2"
+              >
+                <div
+                  className={`size-10 ${index >= 3 ? "bg-slate-400" : index === 2 ? "bg-yellow-700" : index === 1 ? "bg-gray-400" : "bg-yellow-400"} mr-4 flex items-center justify-center rounded-full font-bold text-white`}
+                >
+                  {index + 1}
+                </div>
+                <p className="m-2 font-bold">{user.firstName}</p>
+                <p className="font-bold">
+                  {(user?.totalEarnings ?? 0) < 0 ? "-" : ""}$
+                  {rankType
+                    ? Math.abs(user?.totalEarnings ?? 0)
+                    : Math.abs(
+                        (user?.totalEarnings ?? 0) /
+                          (user.sessionsAttended?.length || 1),
+                      ) + "/day"}
+                </p>
+              </div>
+            ))
+          )}
+        </div>
       </div>
     </div>
   );
