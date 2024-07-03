552fddb28deb495af50431fbacec7e1a6846cd62
anthonyych4n <anthony168c@gmail.com>
2024-07-02 21:43:11 -0600

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,13 @@ import CountdownTimer from "@/components/homepage/CountdownTimer";
 import Podium from "@/components/homepage/Podium";
 
 export default function Home() {
-  const THREE_DAYS_IN_MS = 3 * 24 * 60 * 60 * 1000;
-
-  const NOW_IN_MS = new Date().getTime();
-
-  const dateTimeAfterThreeDays = new Date(NOW_IN_MS + THREE_DAYS_IN_MS);
-
   return (
     <div className="flex min-h-screen w-full flex-row items-center justify-around p-10">
       <div className="">
         <Podium />
       </div>
       <div className="">
-        <CountdownTimer targetDate={dateTimeAfterThreeDays} />
+        <CountdownTimer />
       </div>
     </div>
   );
