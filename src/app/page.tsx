90c956afe93fd01ec458bd7f94f4f24f42c7a1f1
Burton Jong <burtonjong05@gmail.com>
2024-07-10 17:35:14 -0600

    podium/timer isnt shown until user logs in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,27 @@
+"use client";
+
+import { useUser } from "@/components/contexts/UserContext";
 import CountdownTimer from "@/components/homepage/CountdownTimer";
 import Podium from "@/components/homepage/Podium";
 
 export default function Home() {
+  const user = useUser().currentUser;
   return (
-    <div className="flex min-h-screen w-full flex-row items-center justify-around p-10">
-      <div className="">
-        <Podium />
-      </div>
-      <div className="">
-        <CountdownTimer />
-      </div>
-    </div>
+    <>
+      {user.type === "FirstTimeUser" ? (
+        <div className="flex min-h-screen w-full flex-row items-center justify-around p-10">
+          <h1>Login First</h1>
+        </div>
+      ) : (
+        <div className="flex min-h-screen w-full flex-row items-center justify-around p-10">
+          <div className="">
+            <Podium />
+          </div>
+          <div className="">
+            <CountdownTimer />
+          </div>
+        </div>
+      )}
+    </>
   );
 }
